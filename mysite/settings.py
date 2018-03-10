63150a8250a5ae8e4f94ebe80a2fd7b77449566b
marutaku <satouredstone@gmail.com>
2018-03-10 13:54:56 +0900

    delete unnecesarry codes

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&umgl(0r=x0+knza@4*4z!ndo#8m%ut+k%0&_pypdj&(z-^2k%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.33.10']
+ALLOWED_HOSTS = ['192.168.33.10','localhost', '127.0.0.1']
 
 
 # Application definition
